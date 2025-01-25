0a2642d172804515ce590ad6082071796a111eff
Alex Wojtowicz <izabella.wojtowicz@gmail.com>
2025-01-25 14:22:32 +0100

    debug

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,18 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static bool debug = true;
+    public static Action<bool> DebugSwitch;
+    void SwitchDebug()
     {
-        
+        debug = !debug;
+        DebugSwitch?.Invoke(debug);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            if(Input.GetKeyDown(KeyCode.Q)) SwitchDebug();
+        }
         
     }
 }
