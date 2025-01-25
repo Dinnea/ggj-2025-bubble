4be8d9442e0a515f8c5f1d18c5fe0e906d03f5af
Alex Wojtowicz <izabella.wojtowicz@gmail.com>
2025-01-25 14:22:32 +0100

    debug

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -8,4 +8,20 @@ public class Spawner : MonoBehaviour
     {
         Instantiate(toSpawn, transform.position, transform.rotation);
     }
+
+    private void OnEnable()
+    {
+        GameManager.DebugSwitch += DebugMode;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.DebugSwitch -= DebugMode;
+    }
+
+    void DebugMode(bool debug)
+    {
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+        renderer.enabled = debug;
+    }
 }
